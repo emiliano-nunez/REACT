4aa75dfc752a0d7c6ebe654bd35313676481c6a5
emiliano-nunez <emilianoabelnunez@gmail.com>
2024-08-23 19:50:02 -0300

    actualizo la última clase

--- CLASE 60 - Ultima clase/proyecto-vite/src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Input({handleChange}) {
-  return (
-    <input type="text" onChange={handleChange} placeholder='filtrar productos'/>
-  )
-}
-
-export default Input
